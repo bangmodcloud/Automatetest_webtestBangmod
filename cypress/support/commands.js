11e681c6abbd89c8f2ddc4c4f78d18ec396ffe86
PPlaWara <waraporn.s@bangmod.co.th>
2024-11-06 16:16:41 +0700

    [BMS-86cwz8gzu] Subnet / Listing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,7 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import 'cypress-file-upload';
 
 const username = 'plapika03@gmail.com'
 const password = '769461Pla-'
@@ -93,6 +92,50 @@ Cypress.Commands.add('pathKeypair', () => {
 
         cy.wait(1000)
         cy.visit('https://bangmod-dev-web-v2.dev.bangmod.cloud/cloud-server/volume-backup')
+      }
+    )
+  }),
+  Cypress.Commands.add('pathPrivate', () => {
+    cy.session(
+      username,
+      () => {
+        cy.visit('https://bangmod-dev-web-v2.dev.bangmod.cloud/network/private-network')
+        cy.get('a').click();
+        cy.wait(1000)
+        cy.get('input[name=username]').type(username)
+        cy.get('input[name=password]').type(`${password}{enter}`, { log: false })
+
+        cy.get('.otp-field').eq(0).type('1');
+        cy.get('.otp-field').eq(1).type('2');
+        cy.get('.otp-field').eq(2).type('3');
+        cy.get('.otp-field').eq(3).type('4');
+        cy.get('.otp-field').eq(4).type('5');
+        cy.get('.otp-field').eq(5).type('6');
+
+        cy.wait(1000)
+        cy.visit('https://bangmod-dev-web-v2.dev.bangmod.cloud/network/private-network')
+      }
+    )
+  }),
+  Cypress.Commands.add('pathSubnet', () => {
+    cy.session(
+      username,
+      () => {
+        cy.visit('https://bangmod-dev-web-v2.dev.bangmod.cloud/network/subnet')
+        cy.get('a').click();
+        cy.wait(1000)
+        cy.get('input[name=username]').type(username)
+        cy.get('input[name=password]').type(`${password}{enter}`, { log: false })
+
+        cy.get('.otp-field').eq(0).type('1');
+        cy.get('.otp-field').eq(1).type('2');
+        cy.get('.otp-field').eq(2).type('3');
+        cy.get('.otp-field').eq(3).type('4');
+        cy.get('.otp-field').eq(4).type('5');
+        cy.get('.otp-field').eq(5).type('6');
+
+        cy.wait(1000)
+        cy.visit('https://bangmod-dev-web-v2.dev.bangmod.cloud/network/subnet')
       }
     )
   }),
